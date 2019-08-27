da1ac830e071c9054cc4766da3d1734fc4f0cb72
Matthew Flores <flo2312@gmail.com>
2019-08-27 00:48:35 +0000

    Initial commit

--- imageblur3.rb
+++
@@ -1,6 +0,0 @@
-# 1. copy image_blur_2 over to an image_blur_3
-# 2. expand the image to 10x10
-# 3. re-handle the edge cases
-# 4. make blur method accept an argument for the blur distance
-# things to research...
-# Ruby's times method
